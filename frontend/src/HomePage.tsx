5dbaaa6f3b1d4b2dc8fe43e2d26c28c0aeb12634
travis-watson1 <watsonjamest@gmail.com>
2020-07-17 09:15:04 -0500

    Add Page and PageTitle components

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -4,16 +4,12 @@ import { css, jsx } from '@emotion/core';
 import { PrimaryButton } from './Styles';
 import { QuestionList } from './QuestionList';
 import { getUnansweredQuestions } from './QuestionsData';
+import { Page } from './Page';
+import { PageTitle } from './PageTitle';
 
 
 export const HomePage = () => (
-    <div
-        css={css`
-            margin: 50px auto 20px auto;
-            padding: 30px 20px;
-            max-width: 600px;
-        `}
-    >
+    <Page>
         <div
             css={css`
                 display: flex;
@@ -21,19 +17,9 @@ export const HomePage = () => (
                 justify-content: space-between;
             `}
         >
-            <h2
-                css={css`
-                    font-size: 15px;
-                    font-weight: bold;
-                    margin: 10px 0px 5px;
-                    text-align: center;
-                    text-transform: uppercase;
-                `}
-            >
-                Unanswered Questions
-            </h2>
+            <PageTitle>Unanswered Questions</PageTitle>
             <PrimaryButton>Ask a question</PrimaryButton>
         </div>
         <QuestionList data={getUnansweredQuestions()} />
-    </div>
+    </Page>
 );
