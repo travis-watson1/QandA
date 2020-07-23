0b95473cd8c1292c941ac9fcdc046b35a907a850
travis-watson1 <watsonjamest@gmail.com>
2020-07-23 08:05:53 -0500

    Create various actions

--- frontend/src/Store.ts
+++ frontend/src/Store.ts
@@ -1,4 +1,11 @@
-import { QuestionData } from './QuestionsData';
+import {
+  QuestionData,
+  getUnansweredQuestions,
+  postQuestion,
+  PostQuestionData,
+} from './QuestionsData';
+import { Action, ActionCreator, Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk';
 
 interface QuestionsState {
   readonly loading: boolean;
@@ -6,11 +13,77 @@ interface QuestionsState {
   readonly postedResult?: QuestionData;
 }
 
+interface GettingUnansweredQuestionsAction
+  extends Action<'GettingUnansweredQuestions'> {
+  type: 'GettingUnansweredQuestions';
+}
+
 export interface AppState {
   readonly questions: QuestionsState;
 }
 
+export interface GotUnansweredQuestionsAction
+  extends Action<'GotUnansweredQuestions'> {
+  questions: QuestionData[];
+}
+
+export interface PostedQuestionAction extends Action<'PostedQuestion'> {
+  result: QuestionData | undefined;
+}
+
+type QuestionsActions =
+  | GettingUnansweredQuestionsAction
+  | GotUnansweredQuestionsAction
+  | PostedQuestionAction;
+
 const initialQuestionState: QuestionsState = {
   loading: false,
   unanswered: null,
+};
+
+export const getUnansweredQuestionsActionCreator: ActionCreator<ThunkAction<
+  Promise<void>,
+  QuestionData[],
+  null,
+  GotUnansweredQuestionsAction
+>> = () => {
+  return async (dispatch: Dispatch) => {
+    //Dispatch the action
+    const gettingUnansweredQuestionsAction: GettingUnansweredQuestionsAction = {
+      type: 'GettingUnansweredQuestions',
+    };
+    dispatch(gettingUnansweredQuestionsAction);
+    //Await questions from server
+    const questions = await getUnansweredQuestions();
+    //dispatch the action
+    const gotUnansweredQuestionAction: GotUnansweredQuestionsAction = {
+      questions,
+      type: 'GotUnansweredQuestions',
+    };
+    dispatch(gotUnansweredQuestionAction);
+  };
+};
+
+export const postQuestionActionCreator: ActionCreator<ThunkAction<
+  Promise<void>,
+  QuestionData,
+  PostQuestionData,
+  PostedQuestionAction
+>> = (question: PostQuestionData) => {
+  return async (dispatch: Dispatch) => {
+    const result = await postQuestion(question);
+    const postedQuestionAction: PostedQuestionAction = {
+      type: 'PostedQuestion',
+      result,
+    };
+    dispatch(postedQuestionAction);
+  };
+};
+
+export const clearPostedQuestionActionCreator: ActionCreator<PostedQuestionAction> = () => {
+  const postedQuestionAction: PostedQuestionAction = {
+    type: 'PostedQuestion',
+    result: undefined,
+  };
+  return postedQuestionAction;
 };
