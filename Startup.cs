a4f3e436cde5cd39f0ce4237a7088fd77236cfc9
travis-watson1 <watsonjamest@gmail.com>
2020-08-03 11:34:17 -0500

    Add QuestionsController and connect it to DataRepository

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using DbUp;
+using QandA.Data;
 
 namespace QandA
 {
@@ -44,6 +45,9 @@ namespace QandA
 
 
             services.AddControllers();
+
+            services.AddScoped<IDataRepository, DataRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
